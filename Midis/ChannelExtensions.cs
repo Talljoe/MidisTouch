8ed2b3fd57fe442a2a4644804144127013851c66
Joe Wasson <jwasson+git@gmail.com>
2011-03-30 17:11:42 -0700

    Add Split and Splice extension methods.

--- Midis/ChannelExtensions.cs
+++ Midis/ChannelExtensions.cs
@@ -6,6 +6,7 @@ namespace Midis
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
+    using Midis.Utility;
 
     public static class ChannelExtensions
     {
@@ -34,12 +35,14 @@ namespace Midis
             return source.ToChannels(Enumerable.Range(0, channels.Count).Where(i => channels[i]).ToArray());
         }
 
-        public static IObservable<ChannelMessage> ToChannels(this IObservable<ChannelMessage> source, params int[] channels)
+        public static IObservable<ChannelMessage> ToChannels(this IObservable<ChannelMessage> source,
+                                                             params int[] channels)
         {
             return source.ToChannels((IEnumerable<int>) channels);
         }
 
-        public static IObservable<ChannelMessage> ToChannels(this IObservable<ChannelMessage> source, IEnumerable<int> channels)
+        public static IObservable<ChannelMessage> ToChannels(this IObservable<ChannelMessage> source,
+                                                             IEnumerable<int> channels)
         {
             return source.SelectMany(message => channels.ToObservable(),
                                      (message, channel) =>
@@ -59,28 +62,33 @@ namespace Midis
             return source.OnChannels(Enumerable.Range(0, channels.Count).Where(i => channels[i]).ToArray());
         }
 
-        public static IObservable<ChannelMessage> OnChannels(this IObservable<ChannelMessage> source, params int[] channels)
+        public static IObservable<ChannelMessage> OnChannels(this IObservable<ChannelMessage> source,
+                                                             params int[] channels)
         {
-            return source.OnChannels((IEnumerable<int>)channels);
+            return source.OnChannels((IEnumerable<int>) channels);
         }
 
-        public static IObservable<ChannelMessage> OnChannels(this IObservable<ChannelMessage> source, IEnumerable<int> channels)
+        public static IObservable<ChannelMessage> OnChannels(this IObservable<ChannelMessage> source,
+                                                             IEnumerable<int> channels)
         {
             var set = new HashSet<int>(channels);
             return source.Where(message => set.Contains(message.Channel));
         }
 
-        public static IObservable<ChannelMessage> OfMessageType(this IObservable<ChannelMessage> source, ChannelMessageType type)
+        public static IObservable<ChannelMessage> OfMessageType(this IObservable<ChannelMessage> source,
+                                                                ChannelMessageType type)
         {
             return source.Where(message => message.MessageType == type);
         }
 
-        public static IObservable<ChannelMessage> OfMessageTypes(this IObservable<ChannelMessage> source, params ChannelMessageType[] types)
+        public static IObservable<ChannelMessage> OfMessageTypes(this IObservable<ChannelMessage> source,
+                                                                 params ChannelMessageType[] types)
         {
             return source.OfMessageTypes((IEnumerable<ChannelMessageType>) types);
         }
 
-        public static IObservable<ChannelMessage> OfMessageTypes(this IObservable<ChannelMessage> source, IEnumerable<ChannelMessageType> types)
+        public static IObservable<ChannelMessage> OfMessageTypes(this IObservable<ChannelMessage> source,
+                                                                 IEnumerable<ChannelMessageType> types)
         {
             var set = new HashSet<ChannelMessageType>(types);
             return source.Where(message => set.Contains(message.MessageType));
@@ -89,9 +97,9 @@ namespace Midis
         public static IObservable<bool> MomentaryButton(this IObservable<ChannelMessage> source, int controller)
         {
             return source.OfMessageType(ChannelMessageType.ControllerChange)
-                         .Where(message => message.Value1 == controller)
-                         .Select(message => message.Value2 >= 64)
-                         .DistinctUntilChanged();
+                .Where(message => message.Value1 == controller)
+                .Select(message => message.Value2 >= 64)
+                .DistinctUntilChanged();
         }
 
         public static IObservable<bool> ToggleButton(this IObservable<ChannelMessage> source, int controller)
@@ -99,7 +107,8 @@ namespace Midis
             return source.MomentaryButton(controller).Where(b => b).Scan((acc, _) => !acc);
         }
 
-        public static IObservable<ChannelMessage> ToControllerMessage(this IObservable<bool> source, int controller, int channel = 1)
+        public static IObservable<ChannelMessage> ToControllerMessage(this IObservable<bool> source, int controller,
+                                                                      int channel = 1)
         {
             return source.Select(
                 b => new ChannelMessage
@@ -109,6 +118,31 @@ namespace Midis
                              Value1 = controller,
                              Value2 = b ? 127 : 0
                          });
+        }
+
+        public static Tuple<IObservable<T>, IObservable<T>> Split<T>(this IObservable<T> source, Func<T, bool> @switch)
+        {
+            source = source.Publish().RefCount();
+            return new Tuple<IObservable<T>, IObservable<T>>(source.Where(@switch), source.Where(@switch.Not()));
+        }
+
+        public static IObservable<ChannelMessage> Splice(this IObservable<ChannelMessage> source,
+                                                         Func<ChannelMessage, bool> @switch,
+                                                         Func<ChannelMessage, ChannelMessage> selector1,
+                                                         Func<ChannelMessage, ChannelMessage> selector2 = null)
+        {
+            Func<ChannelMessage, ChannelMessage> nullSelector = (x => x);
+            selector1 = selector1 ?? nullSelector;
+            selector2 = selector2 ?? nullSelector;
+            return source.Select(message => @switch(message) ? selector1(message) : selector2(message));
+        }
+
+        public static IObservable<ChannelMessage> Splice(this IObservable<ChannelMessage> source,
+                                                         ChannelMessageType messageType,
+                                                         Func<ChannelMessage, ChannelMessage> selector1,
+                                                         Func<ChannelMessage, ChannelMessage> selector2 = null)
+        {
+            return source.Splice(message => message.MessageType == messageType, selector1, selector2);
         }
     }
 }
---
+++ Midis/Utility/Composition.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2011 Tall Ambitions, LLC
+// See included LICENSE for details.
+namespace Midis.Utility
+{
+    using System;
+
+    public static class CompositionExtensions
+    {
+        public static Func<T, bool> Not<T>(this Func<T, bool> op)
+        {
+            return x => !op(x);
+        }
+
+        public static Func<TRet> Curry<T, TRet>(this Func<T, TRet> f, T value)
+        {
+            return () => f(value);
+        }
+
+        public static Func<T2, TRet> Curry<T1, T2, TRet>(this Func<T1, T2, TRet> f, T1 v1)
+        {
+            return v2 => f(v1, v2);
+        }
+
+        public static Func<T2, T1, TRet> Swap<T1, T2, TRet>(this Func<T1, T2, TRet> f)
+        {
+            return (a, b) => f(b, a);
+        }
+    }
+}
